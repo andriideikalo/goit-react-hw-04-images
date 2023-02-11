e39b45663768413de2755e683e084c777597c21d
andriideikalo <wlada801@gmail.com>
2023-02-11 14:19:05 +0200

    fix

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import { GalleryStyles } from './ImageGalleryStyles';
+import { nanoid } from 'nanoid';
 
-//
+const id = nanoid();
 
 export const ImageGallery = ({ images, onImageClick }) => (
   <GalleryStyles>
     {images.map(image => (
-      <ImageGalleryItem
-        key={image.id}
-        image={image}
-        onImageClick={onImageClick}
-      />
+      <ImageGalleryItem key={id} image={image} onImageClick={onImageClick} />
     ))}
   </GalleryStyles>
 );
